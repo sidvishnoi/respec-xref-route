2a35d88da670a24987e7b6152bcb7af17d0c5794
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-11-22 13:48:50 +0530

    Add scraper (#1)

---
+++ scraper/fix-uri.js
@@ -0,0 +1,73 @@
+// convert long uri to short uri
+// (remove the host part, keep only path)
+// eg: https://html.spec.whatwg.org/multipage/workers.html#abstractworker
+// gets converted to:
+// workers.html#abstractworker
+// as https://html.spec.whatwg.org/multipage/ belongs to `spec-urls.txt`
+
+const { readFileSync } = require("fs");
+const path = require("path");
+
+const URIs = new Set(
+  readFileSync(path.resolve("./scraper/spec-urls.txt"), "utf8")
+    .split("\n")
+    .filter(Boolean),
+);
+
+// We'll use a Trie for an efficient prefix search
+const trie = buildTrie(URIs);
+
+let warningCount = 0;
+module.exports = function fixURI(uri) {
+  if (typeof uri !== "string") {
+    throw new TypeError(`Expected "string", but got "${typeof uri}"`);
+  }
+  let iter = trie;
+  let prefixLength = 0;
+  for (const ch of uri) {
+    ++prefixLength;
+    if (iter.children.has(ch)) {
+      const node = iter.children.get(ch);
+      if (node.isEnd) {
+        return uri.substr(prefixLength);
+      }
+      iter = node;
+    } else {
+      break;
+    }
+  }
+  console.warn(
+    `(fixURI#${++warningCount}): Cannot resolve url: ${uri}. ` +
+      "Please add a base url to spec-urls.txt",
+  );
+  return uri;
+};
+
+/** @param {string[]} strs */
+function buildTrie(strs) {
+  class TrieNode {
+    /** @param {string} ch */
+    constructor(ch) {
+      this.ch = ch;
+      this.isEnd = false;
+      /** @type Map<string, TrieNode> */
+      this.children = new Map();
+    }
+  }
+
+  const rootNode = new TrieNode("@");
+  for (const str of strs) {
+    let temp = rootNode;
+    for (const ch of str) {
+      if (!temp.children.has(ch)) {
+        const node = new TrieNode(ch);
+        temp.children.set(ch, node);
+        temp = node;
+      } else {
+        temp = temp.children.get(ch);
+      }
+    }
+    temp.isEnd = true;
+  }
+  return rootNode;
+}
---
+++ scraper/scraper.js
@@ -0,0 +1,107 @@
+// Reads and parses anchor data files from bikeshed-data repository
+// and writes data.json containing parsed and formatted data
+
+const { readFileSync, readdirSync, writeFileSync } = require("fs");
+const path = require("path");
+const fixURI = require("./fix-uri");
+
+const INPUT_DIR = path.resolve("./bikeshed-data/data/anchors/");
+const OUT_FILE = path.resolve("./data.json");
+
+const SUPPORTED_TYPES = new Set([
+  "attribute",
+  "dict-member",
+  "dictionary",
+  "enum",
+  "enum-value",
+  "interface",
+  "method",
+  "dfn",
+  "event",
+  "element",
+]);
+
+console.log(`Reading files from ${INPUT_DIR}`);
+const fileNames = readdirSync(INPUT_DIR);
+
+const data = fileNames.reduce((data, fileName, i) => {
+  process.stdout.write(`\rProcessing: ${i + 1}/${fileNames.length}`);
+  const file = path.join(INPUT_DIR, fileName);
+  const content = readFileSync(file, "utf8");
+  try {
+    const terms = parseData(content);
+    addTermsToData(terms, data);
+  } catch (error) {
+    console.error(`Error while processing ${fileName}`);
+    throw error;
+  }
+  return data;
+}, Object.create(null));
+
+console.log(`\nWriting data file to ${OUT_FILE}`);
+writeFileSync(OUT_FILE, JSON.stringify(data, null, 2), "utf8");
+
+/**
+ * Parse and format the contents of an anchors file
+ * (https://github.com/tabatkins/bikeshed-data/blob/master/data/anchors/)
+ *
+ * @param {string} content content of an anchors data file
+ *
+ * The parsing is based on the file format specified at
+ * https://github.com/tabatkins/bikeshed/blob/0da7328/bikeshed/update/updateCrossRefs.py#L313-L328
+ */
+function parseData(content) {
+  const sections = content.split("\n-\n").filter(Boolean);
+
+  // format each section to convert data into a usable form
+  const termData = sections
+    .map(section => section.split("\n"))
+    // convert lines (array) to an object for easier access
+    .map(lines => {
+      const [
+        key,
+        type,
+        spec,
+        shortname,
+        level,
+        status,
+        uri,
+        isExported,
+        normative,
+        ..._for
+      ] = lines;
+      const dataFor = _for.filter(Boolean);
+      try {
+        return {
+          key,
+          isExported: isExported === "1",
+          type,
+          spec,
+          shortname,
+          status,
+          uri: fixURI(uri),
+          normative: normative === "1",
+          for: dataFor.length > 0 ? dataFor : undefined,
+        };
+      } catch (error) {
+        console.error("Error while processing section:");
+        console.error(lines);
+        throw error;
+      }
+    });
+
+  const filtered = termData
+    .filter(term => term.isExported && SUPPORTED_TYPES.has(term.type))
+
+  // return unique data
+  const unique = new Set(filtered.map(JSON.stringify));
+  const result = [...unique].map(JSON.parse);
+  return result;
+}
+
+function addTermsToData(terms, data) {
+  for (const { key, isExported, ...termData } of terms) {
+    if (!data[key]) data[key] = [];
+    data[key].push(termData);
+  }
+}
