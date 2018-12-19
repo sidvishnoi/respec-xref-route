f7208ccd135de78422776856be6d1fc0079773a7
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-12-19 18:58:05 +0530

    perf(scraper.js): read/write files async (#16)

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -3,9 +3,13 @@
 // Reads and parses anchor data files from bikeshed-data repository
 // and writes data.json containing parsed and formatted data
 
-const { readFileSync, readdirSync, writeFileSync } = require("fs");
+const { readFile, readdirSync, writeFile } = require("fs");
 const path = require("path");
+const { promisify } = require("util");
 const fixURI = require("./fix-uri");
+
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 const INPUT_DIR = path.resolve("./bikeshed-data/data/anchors/");
 const OUT_FILE = path.resolve("./xref-data.json");
@@ -24,46 +28,59 @@ const SUPPORTED_TYPES = new Set([
   "typedef",
 ]);
 
-console.log(`Reading files from ${INPUT_DIR}`);
-const fileNames = readdirSync(INPUT_DIR);
+async function main() {
+  console.log(`Reading files from ${INPUT_DIR}`);
+  const fileNames = readdirSync(INPUT_DIR);
 
-console.log(`Processing ${fileNames.length} files...`);
-const errorURIs = [];
-const data = fileNames.reduce((data, fileName) => {
-  const file = path.join(INPUT_DIR, fileName);
-  const content = readFileSync(file, "utf8");
-  try {
-    const terms = parseData(content);
-    addTermsToData(terms, data);
-  } catch (error) {
-    console.error(`Error while processing ${fileName}`);
-    throw error;
+  console.log(`Reading ${fileNames.length} files...`);
+  const contentPromises = fileNames.map(fileName => {
+    const file = path.join(INPUT_DIR, fileName);
+    return readFileAsync(file, "utf8");
+  });
+  const content = await Promise.all(contentPromises);
+
+  console.log(`Processing ${fileNames.length} files...`);
+  const errorURIs = [];
+  const data = fileNames.reduce((data, fileName, i) => {
+    try {
+      const terms = parseData(content[i], errorURIs);
+      addTermsToData(terms, data);
+    } catch (error) {
+      console.error(`Error while processing ${fileName}`);
+      throw error;
+    }
+    return data;
+  }, Object.create(null));
+
+  if (errorURIs.length) {
+    console.error(
+      `[fixURI]: Failed to resolve base url. (x${errorURIs.length})`,
+      "Please add base url to spec-urls.txt for the following urls:"
+    );
+    console.error(errorURIs.join("\n"));
+    process.exit(1);
   }
-  return data;
-}, Object.create(null));
 
-if (errorURIs.length) {
-  console.error(
-    `[fixURI]: Failed to resolve base url. (x${errorURIs.length})`,
-    "Please add base url to spec-urls.txt for the following urls:"
-  );
-  console.error(errorURIs.join("\n"));
-  process.exit(1);
+  console.log(`Writing data file to ${OUT_FILE}`);
+  await writeFileAsync(OUT_FILE, JSON.stringify(data, null, 2), "utf8");
 }
 
-console.log(`Writing data file to ${OUT_FILE}`);
-writeFileSync(OUT_FILE, JSON.stringify(data, null, 2), "utf8");
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
 
 /**
  * Parse and format the contents of an anchors file
  * (https://github.com/tabatkins/bikeshed-data/blob/master/data/anchors/)
  *
  * @param {string} content content of an anchors data file
+ * @param {string[]} errorURIs list of uri where fixUri fails
  *
  * The parsing is based on the file format specified at
  * https://github.com/tabatkins/bikeshed/blob/0da7328/bikeshed/update/updateCrossRefs.py#L313-L328
  */
-function parseData(content) {
+function parseData(content, errorURIs) {
   const re = /\r\n|\n\r|\n|\r/g; // because of Windows!
   const normalizedContent = content.replace(re, "\n");
 
