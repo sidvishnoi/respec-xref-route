294cc83b1fffcd9997ac0adc7b04935d32ae59a0
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-04-12 00:23:16 +0530

    BREAKING CHANGE: change response format
    Public API changes:
    - added optional but recommended `hash` field in `RequestEntry`
    - xref response data is moved to `Response.result`
    - added an option to return request query in response (`options.query`)
    
    - We return a result entry corresponding to each query key.
    - This enables caching on server and reduces complexity.
    - Did some refactoring of types also.

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+export interface DataEntry {
+  type: string;
+  spec: string;
+  shortname: string;
+  status: 'snapshot' | 'current';
+  uri: string;
+  normative: boolean;
+  for?: string[];
+}
+
+export interface Database {
+  [term: string]: DataEntry[];
+}
+
+export interface HashCacheEntry {
+  time: number;
+  value: DataEntry[];
+}
+
+export type Cache = Map<'xref', Database> &
+  Map<'cache', Map<string, HashCacheEntry>>;
+
+export interface RequestEntry {
+  term: string;
+  hash: string;
+  types?: string[];
+  specs?: string[];
+  for?: string;
+}
+
+export interface Response {
+  result: DataEntry[];
+  query?: RequestEntry[];
+}
