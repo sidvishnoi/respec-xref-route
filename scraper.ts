dcfd6ad27ef4f7d826231c93b29a724eb9a223f0
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-08-15 14:28:39 +0530

    refactor(search): move cache and its creation in separate file

--- scraper.ts
+++ scraper.ts
@@ -8,7 +8,7 @@ import { resolve as resolvePath, join as joinPath } from 'path';
 import { spawn } from 'child_process';
 import Trie from 'compact-prefix-tree/cjs';
 import { SUPPORTED_TYPES, DATA_DIR } from './constants.js';
-import { Data } from './search.js';
+import { Data } from './cache.js';
 
 const { readdir, readFile, writeFile } = fs;
 
