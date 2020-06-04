306e2fd0d16cc0c7c673664fde8a84ccc489d031
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-06-04 14:21:42 +0530

    feat: support css types

--- constants.ts
+++ constants.ts
@@ -24,11 +24,22 @@ export const IDL_TYPES = new Set([
 // https://github.com/sidvishnoi/respec-xref-route/issues/57
 export const CONCEPT_TYPES = new Set(['_CONCEPT_', 'dfn', 'event', 'element']);
 export const MARKUP_TYPES = new Set(['element', 'element-attr']);
+export const CSS_TYPES_INPUT = new Set([
+  'property',
+  'descriptor',
+  'value',
+  'type',
+  'at-rule',
+  'function',
+  'selector',
+]);
+export const CSS_TYPES = new Set([...CSS_TYPES_INPUT].map(t => `css-${t}`));
 
 export const SUPPORTED_TYPES = new Set([
   ...IDL_TYPES,
   ...CONCEPT_TYPES,
   ...MARKUP_TYPES,
+  ...CSS_TYPES,
 ]);
 
 export const QUERY_CACHE_DURATION = 3 * 24 * 60 * 60 * 1000; // 3 days
