0e9c37fcb00d1a9fd147e53a6014c43447bf7b29
Dominique Hazael-Massieux <dom@w3.org>
2020-12-07 12:02:24 +0100

    BREAKING CHANGE: use definitions data from w3c/webref (#70)
    Co-authored-by: Sid Vishnoi <sidvishnoi8@gmail.com>

--- @types/compact-prefix-tree.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'compact-prefix-tree/cjs' {
-  export { default } from 'compact-prefix-tree';
-  export * from 'compact-prefix-tree';
-}
--- scraper.ts
+++ scraper.ts
@@ -1,20 +1,19 @@
-// Reads and parses anchor data files from bikeshed-data repository and writes:
+// Reads and parses definition data files from webref repository and writes:
 // - xref.json containing parsed and formatted data by term
 // - specs.json having data by spec shortname
 // - specmap.json having spec details
 
 import { promises as fs, existsSync } from 'fs';
 import { resolve as resolvePath, join as joinPath } from 'path';
-import Trie from 'compact-prefix-tree/cjs';
 import { SUPPORTED_TYPES, DATA_DIR, CSS_TYPES_INPUT } from './constants';
 import { sh, uniq } from './utils';
 import { Store } from './store';
+import { Definition as InputDfn, DfnsJSON, SpecsJSON } from 'webref';
 
-const { readdir, readFile, writeFile } = fs;
+const { readFile, writeFile } = fs;
 
-const INPUT_DIR_BASE = joinPath(DATA_DIR, 'bikeshed-data', 'data');
-const SPECS_JSON = resolvePath(INPUT_DIR_BASE, './specs.json');
-const INPUT_ANCHORS_DIR = resolvePath(INPUT_DIR_BASE, './anchors/');
+const INPUT_DIR_BASE = joinPath(DATA_DIR, 'webref', 'ed');
+const SPECS_JSON = resolvePath(INPUT_DIR_BASE, './index.json');
 
 const OUT_DIR_BASE = joinPath(DATA_DIR, 'xref');
 const OUTFILE_BY_TERM = resolvePath(OUT_DIR_BASE, './xref.json');
@@ -46,45 +45,20 @@ export async function main(options: Partial<Options> = {}) {
     return false;
   }
 
-  log(`Reading files from ${INPUT_ANCHORS_DIR}`);
-  const files = await readdir(INPUT_ANCHORS_DIR);
-
-  log(`Reading ${files.length} files...`);
-  const content = await Promise.all(
-    files.map(file => readFile(joinPath(INPUT_ANCHORS_DIR, file), 'utf8')),
-  );
-
-  const { specMap, urls } = await getSpecsMetadata();
-
-  // We'll use a Trie for an efficient prefix search,
-  // to convert long uri to short uri
-  // eg: https://html.spec.whatwg.org/multipage/workers.html#abstractworker
-  // gets converted to:
-  // workers.html#abstractworker
-  // as https://html.spec.whatwg.org/multipage/ belongs to `urls`
-  const trie = new Trie(urls);
+  const { specMap, dfnSources } = await getAllData();
 
   const dataByTerm: DataByTerm = Object.create(null);
   const dataBySpec: DataBySpec = Object.create(null);
-  const errorURIs: string[] = [];
-  log(`Processing ${files.length} files...`);
-  for (let i = 0; i < files.length; i++) {
-    const fileContent = content[i];
+  log(`Processing ${dfnSources.length} files...`);
+  for (const source of dfnSources) {
     try {
-      const terms = parseData(fileContent, errorURIs, trie);
+      const terms = parseData(source);
       updateDataByTerm(terms, dataByTerm);
       updateDataBySpec(terms, dataBySpec);
     } catch (error) {
-      logError(`Error while processing ${files[i]}`);
+      logError(`Error while processing ${source.spec}`);
       throw error;
     }
-  }
-
-  if (errorURIs.length) {
-    // ideally never happens. keeping it to prevent database corruption.
-    const msg = `[fixURI]: Failed to resolve base url. (x${errorURIs.length})`;
-    logError(msg, '\n', errorURIs.join('\n'));
-    process.exit(1);
   }
 
   log('Writing processed data files...');
@@ -99,7 +73,7 @@ export async function main(options: Partial<Options> = {}) {
 async function updateInputSource() {
   const shouldClone = !existsSync(INPUT_DIR_BASE);
   const command = shouldClone
-    ? `git clone https://github.com/tabatkins/bikeshed-data.git --progress`
+    ? `git clone https://github.com/w3c/webref.git --progress`
     : 'git pull origin master';
   const cwd = shouldClone ? DATA_DIR : INPUT_DIR_BASE;
   const stdout = await sh(command, { output: 'stream', cwd });
@@ -108,36 +82,18 @@ async function updateInputSource() {
 }
 
 /**
- * Parse and format the contents of an anchors file
- * <https://github.com/tabatkins/bikeshed-data/blob/master/data/anchors/>
+ * Parse and format the contents of webref dfn files
  *
- * @param content content of an anchors data file
- * @param errorURIs list of uri where fixUri fails
- * @param trie prefix tree for URL resolution
- *
- * The parsing is based on the file format specified at
- * <https://github.com/tabatkins/bikeshed/blob/0da7328/bikeshed/update/updateCrossRefs.py#L313-L328>
+ * @param source content of an dfns data file
  */
-function parseData(content: string, errorURIs: string[], trie: Trie) {
-  const re = /\r\n|\n\r|\n|\r/g; // because of Windows!
-  const normalizedContent = content.replace(re, '\n');
-
+function parseData(source: DfnsJSON) {
+  const { dfns, spec, series, url } = source;
+  const specMetaData = { spec, shortname: series, url };
   const termData = [];
-  for (const section of normalizedContent.split('\n-\n')) {
-    if (!section) continue;
-
-    try {
-      const anchorSection = parseAnchorSection(section);
-      const { prefix, isProper } = trie.prefix(anchorSection.uri);
-      if (!isProper) {
-        errorURIs.push(anchorSection.uri);
-      }
-      anchorSection.uri = anchorSection.uri.replace(prefix, '');
-      termData.push(anchorSection);
-    } catch (error) {
-      logError('Error while processing section:');
-      logError(section);
-      throw error;
+  for (const dfn of dfns) {
+    for (const term of dfn.linkingText) {
+      const mapped = mapDefinition(dfn, term, specMetaData);
+      termData.push(mapped);
     }
   }
 
@@ -148,32 +104,24 @@ function parseData(content: string, errorURIs: string[], trie: Trie) {
   return uniq(filtered);
 }
 
-function parseAnchorSection(section: string) {
-  const [
-    term,
-    type,
-    spec,
-    shortname,
-    level,
-    status,
-    uri,
-    isExported,
-    normative,
-    ...forContext
-  ] = section.split('\n');
-
-  const dataFor = forContext.filter(Boolean);
-  const normalizedType = CSS_TYPES_INPUT.has(type) ? `css-${type}` : type;
+function mapDefinition(
+  dfn: InputDfn,
+  term: string,
+  spec: Record<'spec' | 'shortname' | 'url', string>,
+) {
+  const normalizedType = CSS_TYPES_INPUT.has(dfn.type)
+    ? `css-${dfn.type}`
+    : dfn.type;
   return {
     term: normalizeTerm(term, normalizedType),
-    isExported: isExported === '1',
+    isExported: dfn.access === 'public',
     type: normalizedType,
-    spec,
-    shortname,
-    status,
-    uri, // This is full URL to term here
-    normative: normative === '1',
-    for: dataFor.length > 0 ? dataFor : undefined,
+    spec: spec.spec.toLowerCase(),
+    shortname: spec.shortname.toLowerCase(),
+    status: 'current',
+    uri: dfn.href.replace(spec.url, ''), // This is full URL to term here
+    normative: !dfn.informative,
+    for: dfn.for.length > 0 ? dfn.for : undefined,
   };
 }
 
@@ -205,41 +153,48 @@ function normalizeTerm(term: string, type: string) {
   if (type === 'method' && !term.endsWith(')')) {
     return term + '()';
   }
+  if (type === "dfn") {
+    return term.toLowerCase();
+  }
   return term;
 }
 
-async function getSpecsMetadata() {
-  log(`Getting spec metadata from ${SPECS_JSON}`);
-
-  interface SpecsJSON {
-    [specid: string]: {
-      current_url: string;
-      snapshot_url: string;
-      level: number;
-      title: string;
-      shortname: string;
-    };
-  }
-
-  const urlFileContent = await readFile(SPECS_JSON, 'utf8');
-  const data: SpecsJSON = JSON.parse(urlFileContent);
+async function getAllData() {
+  log(`Getting data from ${SPECS_JSON}`);
+  const urlFileContent = await readJSON(SPECS_JSON);
+  const data: SpecsJSON[] = urlFileContent.results;
 
   const specMap: Store['specmap'] = Object.create(null);
   const specUrls = new Set<string>();
+  const dfnSources: DfnsJSON[] = [];
 
-  for (const [spec, entry] of Object.entries(data)) {
-    if (entry.current_url) specUrls.add(entry.current_url);
-    if (entry.snapshot_url) specUrls.add(entry.snapshot_url);
+  for (const entry of data) {
+    specUrls.add(entry.nightly.url);
+    if (entry.release?.url) specUrls.add(entry.release.url);
+    if (entry.dfns) {
+      const dfnsData = await readJSON(joinPath(INPUT_DIR_BASE, entry.dfns));
+      const dfns: InputDfn[] = dfnsData.dfns;
+      dfnSources.push({
+        series: entry.series.shortname,
+        spec: entry.shortname,
+        url: entry.nightly.url,
+        dfns,
+      });
+    }
 
-    specMap[spec] = {
-      url: entry.current_url || entry.snapshot_url,
+    specMap[entry.shortname.toLowerCase()] = {
+      url: entry.nightly.url || entry.release?.url || entry.url,
       title: entry.title,
-      shortname: entry.shortname,
+      shortname: entry.series.shortname.toLowerCase(),
     };
   }
 
-  const urls = [...specUrls].sort();
-  return { urls, specMap };
+  return { specMap, dfnSources };
+}
+
+async function readJSON(filePath: string) {
+  const text = await readFile(filePath, 'utf-8');
+  return JSON.parse(text);
 }
 
 if (require.main === module) {
